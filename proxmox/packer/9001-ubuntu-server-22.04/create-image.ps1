3d6eff62fc6ad023448366be60921fddedd1e025
Anish <anish@codeanish.com>
2023-08-18 16:34:52 -0700

    Still not working process

--- proxmox/packer/9001-ubuntu-server-22.04/create-image.ps1
+++ proxmox/packer/9001-ubuntu-server-22.04/create-image.ps1
@@ -1,15 +1,18 @@
 
-# Get SSH Public Key
-$variables = Get-Content -Path "../variables.auto.pkrvars.hcl" -Raw
+# Replace ssh_public_key in user-data.template with the value from variables.auto.pkrvars.hcl
+
+$variables = Get-Content -Path "../variables.pkrvars.hcl" -Raw
 
 foreach ($line in $variables.Split("`n")) {
     if ($line -like "*ssh_public_key*") {
         $ssh_public_key = $line.Split("=")[1].Trim()
+        $ssh_public_key = $ssh_public_key.Replace('"', "")
     }
 }
-# Write-Host "ssh_public_key: $ssh_public_key"
-
 $user_data = Get-Content -Path "http/user-data.template" -Raw
 $user_data = $user_data.Replace("{{ssh_public_key}}", $ssh_public_key)
 Out-File -FilePath "http/user-data" -InputObject $user_data -Encoding utf8
-# Write-Host "user_data: $user_data"
+
+# Build the image
+
+packer build -var-file="../variables.pkrvars.hcl" ubuntu-server-22.04.pkr.hcl
