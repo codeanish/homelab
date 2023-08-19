6b5f80508b8d01913824d7806a02666c541d4eb4
codeanish <anish@codeanish.com>
2023-08-18 21:14:40 -0700

    Adguard home initial commit

---
+++ proxmox/terraform/101-adguard-home.tf
@@ -0,0 +1,28 @@
+resource "proxmox_vm_qemu" "adguard-home" {
+    
+    # VM General Settings
+    target_node = "proxmox"
+    vmid = "101"
+    name = "adguard-home"
+    desc = "Ubuntu Server installation of adguard home"
+    clone = "ubuntu-server-22.04"
+    onboot = true 
+    agent = 1
+    
+    # VM Specs
+    cores = 4
+    sockets = 1
+    cpu = "host"    
+    memory = 4096
+
+    # Networking
+    network {
+        bridge = "vmbr1"
+        model  = "virtio"
+    }
+
+    # Cloud init
+    os_type = "cloud-init"
+    ipconfig0 = "ip=10.0.20.88/24,gw=10.0.20.1"
+    ciuser = "anish"
+}
